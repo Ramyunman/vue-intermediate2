018202c5b165bb14c6fbab6cac13ad6de3d707d8
Ramyunman <100078669+Ramyunman@users.noreply.github.com>
2023-12-15 13:43:49 +0900

    231215 helper까지 완료

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -6,10 +6,6 @@
         </span>
 
         <AlertModal v-if="showModal" @close="showModal = false">
-            <!--
-            you can use custom content here to overwrite
-            default content
-            -->
             <h3 slot="header">
                 경고
                 <i class="closeModalBtn fa fa-times"
